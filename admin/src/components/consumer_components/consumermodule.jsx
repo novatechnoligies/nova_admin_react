365b2e0da64654cbd610c48c2909198fec196529
Santosh <78397565+santoshsh2307@users.noreply.github.com>
2023-10-21 08:35:37 +0530

    add other data in form

--- admin/src/components/consumer_components/consumermodule.jsx
+++ admin/src/components/consumer_components/consumermodule.jsx
@@ -1,99 +1,61 @@
-// src/Page.js
 import React, { useState } from 'react';
-import { Button, Modal } from 'antd';
-import { Form, Input, Radio, DatePicker, Checkbox, Upload } from 'antd';
-const { TextArea } = Input;
+import { Button, Modal, Form, Input, Upload } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
-import "../consumer_components/consumermodule.css";
-
+import axios from 'axios';
 
 function NewConsumer() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [form] = Form.useForm(); // Create a form instance
 
   const showModal = () => {
     setIsModalOpen(true);
   };
 
   const handleOk = () => {
-    setIsModalOpen(false);
+    form.validateFields().then((values) => {
+      // Validate form fields and post data if validation is successful
+      console.log('Form values:', values);
+
+      axios.post('/api/consumer/create', values)
+        .then((response) => {
+          console.log('API response:', response);
+          setIsModalOpen(false);
+        })
+        .catch((error) => {
+          console.error('API error:', error);
+        });
+    });
   };
 
   const handleCancel = () => {
     setIsModalOpen(false);
   };
-  
+
   return (
     <div>
-    <Button type="primary" style={{ float: 'right', marginTop:'20px'}} onClick={showModal}>Create Consumer Account</Button>
-      <Modal title="New Consumer Account" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <div style={{justifyContent:'flex-end'}}>
-          <form>
-          <Form.Item name="id" style={{ display: 'none' }}>
-        <Input />
-        </Form.Item>
-        <Form.Item name="createdAt" style={{ display: 'none' }}>
-        <Input />
-        </Form.Item>
-      <Form.Item name="status" style={{ display: 'none' }}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="isDeleted" style={{ display: 'none' }}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="ownerId" style={{ display: 'none' }}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="isPhoneNoVerified" style={{ display: 'none' }}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="role" style={{ display: 'none' }}>
-        <Input />
-      </Form.Item>
-
-      {/* Visible Fields */}
-      <Form.Item name="firstName" label="First Name" rules={[{ required: true, max: 50 }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="lastName" label="Last Name:" rules={[{ max: 50 }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="email" label="Email" rules={[{ required: true, type: 'email', max: 50 }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="phone" label="Phone" rules={[{ required: true, pattern: /^\d{12}$/, message: 'Please enter a valid 12-digit phone number' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="pin" label="Pin" rules={[{pattern: /^\d{6}$/, message: 'Please enter a valid 6-digit pin' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="gender" label="Gender">
-        <Radio.Group>
-          <Radio value="male">Male</Radio>
-          <Radio value="female">Female</Radio>
-          <Radio value="other">Other</Radio>
-        </Radio.Group>
-      </Form.Item>
-      <Form.Item name="username" label="Username" rules={[{ required: true, max: 50 }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="password" label="Password" rules={[{ required: true, max: 20 }]}>
-        <Input.Password />
-      </Form.Item>
-      <Form.Item name="adharNo" label="Aadhar Number" rules={[{ required: true, pattern: /^\d{12}$/, message: 'Please enter a valid 12-digit Aadhar number' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="adharPhoto" label="Aadhar Photo" rules={[{ required: true, max: 255 }]}>
-      <Upload>
-          <Button icon={<UploadOutlined />}>Upload Aadhar Photo</Button>
-        </Upload>
-      </Form.Item>
-      <Form.Item name="ownerPhoto" label="Owner Photo" rules={[{ required: true, max: 255 }]}>
-      <Upload>
-          <Button icon={<UploadOutlined />}>Upload Owner Photo</Button>
-        </Upload>
-      </Form.Item>
-          </form>
-        </div>
+      <Button type="primary" style={{ float: 'right', marginTop: '20px' }} onClick={showModal}>
+        Create Consumer Account
+      </Button>
+      <Modal title="New Consumer Account" visible={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <Form form={form} name="newConsumerForm">
+          <Form.Item
+            name="firstName"
+            label="First Name"
+            rules={[{ required: true, max: 50 }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="lastName"
+            label="Last Name"
+            rules={[{ max: 50 }]}
+          >
+            <Input />
+          </Form.Item>
+          
+          
+            
+        </Form>
       </Modal>
     </div>
   );
