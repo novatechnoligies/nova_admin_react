d1c2078a0ba8d8dc80e616acb0aea9532a2017a9
Santosh <78397565+santoshsh2307@users.noreply.github.com>
2023-10-21 11:45:34 +0530

    yes

--- admin/src/components/consumer_components/consumermodule.jsx
+++ admin/src/components/consumer_components/consumermodule.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Button, Modal, Form, Input, Upload } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import axios from 'axios';
+import { BASE_URL } from "../../constants/constants";
 
 function NewConsumer() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -14,9 +15,13 @@ function NewConsumer() {
   const handleOk = () => {
     form.validateFields().then((values) => {
       // Validate form fields and post data if validation is successful
-      console.log('Form values:', values);
+      const modifiedData = {
+        ...values,
+        role: { id: values.role }
+      };
+      console.log('Form values:', modifiedData);
 
-      axios.post('/api/consumer/create', values)
+      axios.post(BASE_URL+'/api/v1/saveUserDetails', values)
         .then((response) => {
           console.log('API response:', response);
           setIsModalOpen(false);
@@ -38,6 +43,12 @@ function NewConsumer() {
       </Button>
       <Modal title="New Consumer Account" visible={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
         <Form form={form} name="newConsumerForm">
+        <Form.Item
+            name="id"
+            label="id"
+          >
+            <Input />
+        </Form.Item>
           <Form.Item
             name="firstName"
             label="First Name"
@@ -49,6 +60,77 @@ function NewConsumer() {
             name="lastName"
             label="Last Name"
             rules={[{ max: 50 }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[{ required: true, max: 50 }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="phone"
+            label="phone"
+            rules={[{ required: true, max: 50 }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="pin"
+            label="pin"
+            rules={[{ required: true, max: 50 }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="gender"
+            label="gender"
+            rules={[{ required: true, max: 50 }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="username"
+            label="username"
+            rules={[{ required: true, max: 50 }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            label="password"
+            rules={[{ required: true, max: 50 }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="adharNo"
+            label="adharNo"
+            rules={[{ required: true, max: 50 }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="adharPhoto"
+            label="adharPhoto"
+            rules={[{ required: true, max: 50 }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="role"
+            label="role"
+            rules={[{ required: true, max: 50 }]}
           >
             <Input />
           </Form.Item>
