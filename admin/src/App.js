653b67e684eab2aaa3c0fd135f565e8c65a5ebb9
raviroogi <50064611+raviroogi@users.noreply.github.com>
2024-01-06 16:25:03 +0530

    Testing - Removed Icons Count
    Testing, please do not approve and merge.

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,26 +1,32 @@
-import React, { useState } from 'react';
-import { Menu } from 'antd';
-import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
-import { DashboardOutlined, HomeOutlined, ProfileOutlined, SettingOutlined, ShopOutlined, ShoppingOutlined,IdcardOutlined } from '@ant-design/icons/lib/icons';
-import ShopTable from './components/shop_components/ShopTable';
-import SpaTable from './components/spa_components/SpaTable';
-import LabTable from './components/lab_components/LabTable';
-import CscTable from './components/csc_components/CscTable';
-import ClinicTable from './components/clinic_components/ClinicTable';
-import LoginPage from './components/login_components/Login';
+import React, { useState } from "react";
+import { Menu } from "antd";
+import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
+import {
+  DashboardOutlined,
+  HomeOutlined,
+  ProfileOutlined,
+  SettingOutlined,
+  ShopOutlined,
+  ShoppingOutlined,
+  IdcardOutlined,
+} from "@ant-design/icons/lib/icons";
+import ShopTable from "./components/shop_components/ShopTable";
+import SpaTable from "./components/spa_components/SpaTable";
+import LabTable from "./components/lab_components/LabTable";
+import CscTable from "./components/csc_components/CscTable";
+import ClinicTable from "./components/clinic_components/ClinicTable";
+import LoginPage from "./components/login_components/Login";
 // import Filter from './components/filter_componets/Filter';
-import Acards from './components/consumers_appointments/Acards';
-import { NotificationOutlined ,UserOutlined } from '@ant-design/icons';
-import { MailOutlined } from '@ant-design/icons';
-import { Badge, Avatar, Upload, Button, Popover} from 'antd';
+import Acards from "./components/consumers_appointments/Acards";
+import { NotificationOutlined, UserOutlined } from "@ant-design/icons";
+import { MailOutlined } from "@ant-design/icons";
+import { Badge, Avatar, Upload, Button, Popover } from "antd";
 import "./App.css";
-import NewConsumer from './components/consumer_components/consumermodule';
-import { More } from './components/consumers_appointments/More';
-import AppointmentBookingPage from './components/consumers_appointments/AppointmentBookingPage';
-import defaultProfilePhoto from './default-profile-photo.png';
-import AppointmentDetails from './components/consumers_appointments/AppointmentDetails';
-
-
+import NewConsumer from "./components/consumer_components/consumermodule";
+import { More } from "./components/consumers_appointments/More";
+import AppointmentBookingPage from "./components/consumers_appointments/AppointmentBookingPage";
+import defaultProfilePhoto from "./default-profile-photo.png";
+import AppointmentDetails from "./components/consumers_appointments/AppointmentDetails";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -41,17 +47,25 @@ function App() {
     <div>
       {loggedIn ? (
         <section>
-           <div> 
-          <Header onLogout={handleLogout} />
+          <div>
+            <Header onLogout={handleLogout} />
           </div>
           <div className="wrapper">
             <div className="container">
-              <div className=" left-side"><SlideMenu location={location} navigate={navigate} onLogout={handleLogout} /></div>
-              <div  className='right-side'>
-                 <div className='container-row'>
-                    {/* <div className="top"><Filter></Filter></div> */}
-                    <div className><Content /></div>
-                 </div>
+              <div className=" left-side">
+                <SlideMenu
+                  location={location}
+                  navigate={navigate}
+                  onLogout={handleLogout}
+                />
+              </div>
+              <div className="right-side">
+                <div className="container-row">
+                  {/* <div className="top"><Filter></Filter></div> */}
+                  <div className>
+                    <Content />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -64,28 +78,31 @@ function App() {
   );
 }
 
-function Header({ onLogout, profilePhoto  }) {
+function Header({ onLogout, profilePhoto }) {
   return (
-    <div  className="top-bar" >
+    <div className="top-bar">
       {/* <div>Header</div> */}
-      <div className='top_bar_icons' >
-        <Badge count='5' offset={[10, 0]}>
-        <NotificationOutlined />
+      <div className="top_bar_icons">
+        <Badge offset={[10, 0]}>
+          <NotificationOutlined />
         </Badge>
       </div>
-      <div  className='top_bar_icons' >
-        <Badge count='5' offset={[10, 0]}>
-        <MailOutlined style={{ fontSize: '24px' }} />
+      <div className="top_bar_icons">
+        <Badge offset={[10, 0]}>
+          <MailOutlined style={{ fontSize: "24px" }} />
         </Badge>
       </div>
-      <div className='top_bar_icons'>
+      <div className="top_bar_icons">
         {profilePhoto ? (
           <img src={profilePhoto} alt="Profile" className="profile-photo" />
         ) : (
-          <img src={defaultProfilePhoto} alt="Default Profile" className="profile-photo" />
+          <img
+            src={defaultProfilePhoto}
+            alt="Default Profile"
+            className="profile-photo"
+          />
         )}
       </div>
-      
     </div>
   );
 }
@@ -95,31 +112,51 @@ function SlideMenu({ location, navigate, onLogout }) {
 
   return (
     <div className="App">
-      <Menu style={{background:'#0B8C73',color:'white', height:'92.5vh', overflow:'hidden'}}
+      <Menu
+        style={{
+          background: "#0B8C73",
+          color: "white",
+          height: "92.5vh",
+          overflow: "hidden",
+        }}
         selectedKeys={selectedKeys}
         onClick={({ key }) => {
-          if (key === '/logout') {
+          if (key === "/logout") {
             onLogout();
           } else {
             navigate(key);
           }
         }}
         items={[
-          { label: 'Home', key: '/', icon: <HomeOutlined /> },
-          { label: 'Dashboard', key: '/dash', icon: <DashboardOutlined /> },
-          { label:'Appointments', key:'Acard',icon: <IdcardOutlined />},
-          { label: 'Lab', key: '/lab', icon: <ShopOutlined /> },
-          { label: 'Spa', key: '/spa', icon: <ShopOutlined /> },
-          { label: 'Shalon', key: '/shalon', icon: <ShopOutlined /> },
-          { label: 'CSC', key: '/csc', icon: <ShopOutlined /> },
-          { label: 'Clinic', key: '/clinic', icon: <ShopOutlined /> },
-          { label: 'Consumer', key: '/consumer', icon: <ShoppingOutlined />, children: [
-          { label: 'Active', key: '/active', icon: <ShopOutlined /> },
-          { label: 'Inactive', key: '/inactive', icon: <ShopOutlined /> },
-          { label: 'New Consumer Account', key: '/newconsumeraccount', icon: <ShopOutlined /> },
-          ] },
-          { label: 'Profile', key: '/profile', icon: <ProfileOutlined /> },
-          { label: 'Sign Out', key: '/logout', icon: <SettingOutlined />, danger: true },
+          { label: "Home", key: "/", icon: <HomeOutlined /> },
+          { label: "Dashboard", key: "/dash", icon: <DashboardOutlined /> },
+          { label: "Appointments", key: "Acard", icon: <IdcardOutlined /> },
+          { label: "Lab", key: "/lab", icon: <ShopOutlined /> },
+          { label: "Spa", key: "/spa", icon: <ShopOutlined /> },
+          { label: "Shalon", key: "/shalon", icon: <ShopOutlined /> },
+          { label: "CSC", key: "/csc", icon: <ShopOutlined /> },
+          { label: "Clinic", key: "/clinic", icon: <ShopOutlined /> },
+          {
+            label: "Consumer",
+            key: "/consumer",
+            icon: <ShoppingOutlined />,
+            children: [
+              { label: "Active", key: "/active", icon: <ShopOutlined /> },
+              { label: "Inactive", key: "/inactive", icon: <ShopOutlined /> },
+              {
+                label: "New Consumer Account",
+                key: "/newconsumeraccount",
+                icon: <ShopOutlined />,
+              },
+            ],
+          },
+          { label: "Profile", key: "/profile", icon: <ProfileOutlined /> },
+          {
+            label: "Sign Out",
+            key: "/logout",
+            icon: <SettingOutlined />,
+            danger: true,
+          },
         ]}
       />
     </div>
@@ -128,26 +165,86 @@ function SlideMenu({ location, navigate, onLogout }) {
 
 function Content() {
   return (
-    <div style={{marginTop:'1px'}}>
+    <div style={{ marginTop: "1px" }}>
       <Routes>
         <Route path="/" element={<div>Dashboard</div>} />
         <Route path="/dash" element={<div>Dashboard</div>} />
-        <Route path="/lab" element={<div className="d-flex flex-column align-items-center"> <LabTable /></div>} />
-        <Route path="/Acard" element={<div className="d-flex flex-column align-items-center"> <Acards/></div>} />
-        <Route path="/spa" element={<div className="d-flex flex-column align-items-center"> <SpaTable /></div>} />
-        <Route path="/shalon" element={<div className="d-flex flex-column align-items-center"> <ShopTable /></div>} />
-        <Route path="/csc" element={<div className="d-flex flex-column align-items-center"> <CscTable /></div>} />
-        <Route path="/clinic" element={<div className="d-flex flex-column align-items-center"> <ClinicTable /></div>} />
+        <Route
+          path="/lab"
+          element={
+            <div className="d-flex flex-column align-items-center">
+              {" "}
+              <LabTable />
+            </div>
+          }
+        />
+        <Route
+          path="/Acard"
+          element={
+            <div className="d-flex flex-column align-items-center">
+              {" "}
+              <Acards />
+            </div>
+          }
+        />
+        <Route
+          path="/spa"
+          element={
+            <div className="d-flex flex-column align-items-center">
+              {" "}
+              <SpaTable />
+            </div>
+          }
+        />
+        <Route
+          path="/shalon"
+          element={
+            <div className="d-flex flex-column align-items-center">
+              {" "}
+              <ShopTable />
+            </div>
+          }
+        />
+        <Route
+          path="/csc"
+          element={
+            <div className="d-flex flex-column align-items-center">
+              {" "}
+              <CscTable />
+            </div>
+          }
+        />
+        <Route
+          path="/clinic"
+          element={
+            <div className="d-flex flex-column align-items-center">
+              {" "}
+              <ClinicTable />
+            </div>
+          }
+        />
         <Route path="/consumer" element={<div>Consumer</div>} />
         <Route path="/profile" element={<div>Profile</div>} />
         <Route path="/logout" element={<div>Logout</div>} />
         <Route path="/active" element={<div>Active</div>} />
         <Route path="/inactive" element={<div>Inactive</div>} />
-        <Route path="/AppChildCards/:id" element={<More/>}/>
-        <Route path="/newconsumeraccount" element={<div><NewConsumer/></div>} />
-        <Route path="/appointment-booking" element={<AppointmentBookingPage />} />
-        <Route path="/appointment-details/:name" element={<AppointmentDetails />} />
-
+        <Route path="/AppChildCards/:id" element={<More />} />
+        <Route
+          path="/newconsumeraccount"
+          element={
+            <div>
+              <NewConsumer />
+            </div>
+          }
+        />
+        <Route
+          path="/appointment-booking"
+          element={<AppointmentBookingPage />}
+        />
+        <Route
+          path="/appointment-details/:name"
+          element={<AppointmentDetails />}
+        />
       </Routes>
     </div>
   );
