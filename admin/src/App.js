48e24f805fda844067e6c17fb549a00aa4244bb3
Sandip Romade <77398564+superearth2021@users.noreply.github.com>
2024-01-10 11:34:32 +0530

    Removed_spa_saloon_csc
    Removed Spa, Saloon and CSC labels and routers. Added the font size of Notification Icon.

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,10 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Menu } from 'antd';
 import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
 import { DashboardOutlined, HomeOutlined, ProfileOutlined, SettingOutlined, ShopOutlined, ShoppingOutlined,IdcardOutlined } from '@ant-design/icons/lib/icons';
-import ShopTable from './components/shop_components/ShopTable';
-import SpaTable from './components/spa_components/SpaTable';
 import LabTable from './components/lab_components/LabTable';
-import CscTable from './components/csc_components/CscTable';
 import ClinicTable from './components/clinic_components/ClinicTable';
 import LoginPage from './components/login_components/Login';
 // import Filter from './components/filter_componets/Filter';
@@ -123,7 +120,7 @@ function Header({ onLogout, profilePhoto  }) {
       {/* <div>Header</div> */}
       <div className='top_bar_icons' >
         <Badge count='5' offset={[10, 0]}>
-        <NotificationOutlined />
+        <NotificationOutlined style={{ fontSize: '24px' }} />
         </Badge>
       </div>
       <div  className='top_bar_icons' >
@@ -162,9 +159,6 @@ function SlideMenu({ location, navigate, onLogout }) {
           { label: 'Dashboard', key: '/dash', icon: <DashboardOutlined /> },
           { label:'Appointments', key:'Acard',icon: <IdcardOutlined />},
           { label: 'Lab', key: '/lab', icon: <ShopOutlined /> },
-          { label: 'Spa', key: '/spa', icon: <ShopOutlined /> },
-          { label: 'Shalon', key: '/shalon', icon: <ShopOutlined /> },
-          { label: 'CSC', key: '/csc', icon: <ShopOutlined /> },
           { label: 'Clinic', key: '/clinic', icon: <ShopOutlined /> },
           { label: 'Consumer', key: '/consumer', icon: <ShoppingOutlined />, children: [
           { label: 'Active', key: '/active', icon: <ShopOutlined /> },
@@ -187,9 +181,6 @@ function Content() {
         <Route path="/dash" element={<div>Dashboard</div>} />
         <Route path="/lab" element={<div className="d-flex flex-column align-items-center"> <LabTable /></div>} />
         <Route path="/Acard" element={<div className="d-flex flex-column align-items-center"> <Acards/></div>} />
-        <Route path="/spa" element={<div className="d-flex flex-column align-items-center"> <SpaTable /></div>} />
-        <Route path="/shalon" element={<div className="d-flex flex-column align-items-center"> <ShopTable /></div>} />
-        <Route path="/csc" element={<div className="d-flex flex-column align-items-center"> <CscTable /></div>} />
         <Route path="/clinic" element={<div className="d-flex flex-column align-items-center"> <ClinicTable /></div>} />
         <Route path="/consumer" element={<div>Consumer</div>} />
         <Route path="/profile" element={<div>Profile</div>} />
