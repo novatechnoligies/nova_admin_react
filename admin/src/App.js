fdae638fb16551890c7abec1a51befb3fcafc5d6
Sandip Romade <77398564+superearth2021@users.noreply.github.com>
2024-01-16 13:11:51 +0530

    test

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Menu } from "antd";
 import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
-import {
+import { 
   DashboardOutlined,
   HomeOutlined,
   ProfileOutlined,
