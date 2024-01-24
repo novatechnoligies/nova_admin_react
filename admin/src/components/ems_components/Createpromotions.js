a1bda8d0159f740ad1f233280f2dccb5a238f215
kadappa gunjiganvi <kadappa.gunjiganvi1@gmail.com>
2024-01-24 17:20:25 +0530

    Ems employed table
    employ management sytem

---
+++ admin/src/components/ems_components/Createpromotions.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Createpromotions = () => {
+    return (
+        <div>
+            <h2>Create Promotions System</h2>
+        </div>
+    )
+}
+
+export default Createpromotions;
