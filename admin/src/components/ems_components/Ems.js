e58dff40995675b07d9f28c1722ed1cc96c0125a
Santosh <78397565+santoshsh2307@users.noreply.github.com>
2024-01-28 05:18:41 +0530

    updated EMS

--- admin/src/components/ems_components/Ems.js
+++ admin/src/components/ems_components/Ems.js
@@ -1,27 +1,95 @@
-import React, { useState } from 'react';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from 'react';
+import { Button, Input } from 'antd';
+import DataTable from 'react-data-table-component';
+import axios from 'axios';
+import { BASE_URL } from '../../constants/constants';
+
 import './Ems.css';
 
 const Ems = () => {
-  // Sample employee data
-  const initialEmployees = [
-    { id:'1', name: 'Krishna', Phone_Number:'8874646783',Address:'Bangalore', Email:'krishna111d@gmail.com',
-      Pincode:'560008',Adhar_Number:'776865673456',position: 'Tecnisian',Gender:'Male' },
-    { id:'2', name: 'Ramakant', Phone_Number:'8546786342',Address:'Bangalore', Email:'ramakant222@gmail.com',
-      Pincode:'560001',Adhar_Number:'887999556633',position: 'Cashier',Gender:'Male' },
-      { id:'3', name: 'Shankar P', Phone_Number:'9567843567',Address:'Bangalore', Email:'shankarp333@gmail.com',
-      Pincode:'530068',Adhar_Number:'6677455443322',position: 'Developer',Gender:'Male' },
-      { id:'4', name: 'Parvatidevi', Phone_Number:'6785467434',Address:'Bangalore', Email:'parvatid444@gmail.com',
-      Pincode:'562157',Adhar_Number:'555447778899',position: 'Designer',Gender:'Female' },
-      { id:'5', name: 'Shivakumar', Phone_Number:'7453287895',Address:'Bangalore', Email:'shivak555@gmail.com',
-      Pincode:'562162',Adhar_Number:'999777556832',position: 'Admin',Gender:'Male' },
-      { id:'6', name: 'Kavita R', Phone_Number:'9867534569',Address:'Bangalore', Email:'kavitar666@gmail.com',
-      Pincode:'570075',Adhar_Number:'8877755443322',position: 'Developer',Gender:'Female' },
-      { id:'7', name: 'Sarswati M', Phone_Number:'8874646783',Address:'Bangalore', Email:'sarswatim456@gmail.com',
-      Pincode:'570078',Adhar_Number:'776865673456',position: 'Developer',Gender:'Female' },
-    // Add more employee data as needed
+  const [search, setSearch] = useState('');
+  const [employeeData, setEmployeeData] = useState([]);
+
+  useEffect(() => {
+    getEmployeeTableData();
+  }, [search]);
+
+  const getEmployeeTableData = async () => {
+    const storedUserData = sessionStorage.getItem('userData');
+    const userDataObject = JSON.parse(storedUserData);
+
+    try {
+      const response = await axios.get(
+        BASE_URL + `/dataservice/getAllLabListByOwnerId?ownerId=${userDataObject.id}`
+      );
+      setEmployeeData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const columns = [
+    {
+      name: 'Lab Name',
+      selector: (row) => row.shopName,
+      sortable: true,
+      style: {
+        fontWeight: 'bold',
+        wordWrap: 'break-word',
+      },
+    },
+    {
+      name: 'Address',
+      selector: (row) => row.shopAddress,
+      sortable: true,
+      style: { wordWrap: 'break-word' },
+    },
+    {
+      name: 'Phone',
+      selector: (row) => row.phone,
+      sortable: true,
+      style: { wordWrap: 'break-word' },
+    },
+    {
+      name: 'Email',
+      selector: (row) => row.email,
+      sortable: true,
+      style: { wordWrap: 'break-word' },
+    },
+    {
+      name: 'pinCode',
+      selector: (row) => row.pinCode,
+      sortable: true,
+      style: { wordWrap: 'break-word' },
+    },
+    {
+      name: 'Ownername',
+      selector: (row) => row.shopName,
+      sortable: true,
+      style: { wordWrap: 'break-word' },
+    },
+    {
+      name: 'GST NO',
+      selector: (row) => row.gstNo,
+      sortable: true,
+      style: { wordWrap: 'break-word' },
+    },
+    {
+      name: 'Action',
+      cell: (row) => (
+        <div>
+          <Button type="link" onClick={() => handleEditClick(row.id)} icon={<EditOutlined />} />
+          <Button type="link" onClick={() => handleDeleteClick(row.id)} icon={<DeleteOutlined />} />
+        </div>
+      ),
+      style: {
+        fontWeight: 'bold',
+        wordWrap: 'break-word',
+      },
+    },
   ];
 
-  const [employees, setEmployees] = useState(initialEmployees);
   const handleEditClick = (employeeId) => {
     // Implement the logic for editing an employee
     console.log(`Edit button clicked for employee with ID ${employeeId}`);
@@ -29,50 +97,42 @@ const Ems = () => {
 
   const handleDeleteClick = (employeeId) => {
     // Implement the logic for deleting an employee
-    const updatedEmployees = employees.filter((employee) => employee.id !== employeeId);
-    setEmployees(updatedEmployees);
+    const updatedEmployeeData = employeeData.filter((employee) => employee.id !== employeeId);
+    setEmployeeData(updatedEmployeeData);
   };
-  
-return (
+
+  return (
     <div className="Ems">
-      <h1>Employee Table</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Phone Number</th>
-            <th>Address</th>
-            <th>Email</th>
-            <th>Pincode</th>
-            <th>Adhar Number</th>
-            <th>Position</th>
-            <th>Gender</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employees.map((employee) => (
-            <tr key={employee.id}>
-              <td>{employee.id}</td>
-              <td>{employee.name}</td>
-              <td>{employee.Phone_Number}</td>
-              <td>{employee.Address}</td>
-              <td>{employee.Email}</td>
-              <td>{employee.Pincode}</td>
-              <td>{employee.Adhar_Number}</td>
-              <td>{employee.position}</td>
-              <td>{employee.Gender}</td>
-              <td>
-            <button style={{ backgroundColor: 'blue', color: 'white' }} onClick={() => handleEditClick(employee.id)}>Edit </button>
-              {' '}
-              <button style={{ backgroundColor: 'blue', color: 'white' }} onClick={() => handleDeleteClick(employee.id)}>Delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <DataTable
+        className="container custom-table lab-data-table-container"
+        title=""
+        columns={columns}
+        data={employeeData}
+        pagination
+        paginationPosition="bottom"
+        fixedHeader
+        selectableRows
+        selectableRowsHighlight
+        highlightOnHover
+        customStyles={{
+          pagination: {
+            marginBottom: '16px', // Adjust the margin bottom as needed
+          },
+        }}
+        subHeaderComponent={
+          <div>
+            <Input
+              type="text"
+              placeholder="Search Here"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+        }
+        subHeader
+      />
     </div>
   );
 };
+
 export default Ems;
--- admin/src/components/lab_components/LabTable.jsx
+++ admin/src/components/lab_components/LabTable.jsx
@@ -1,16 +1,6 @@
 import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
-import {
-  Button,
-  Modal,
-  Form,
-  Input,
-  Select,
-  DatePicker,
-  Checkbox,
-  TimePicker,
-  Card,
-} from "antd";
+import { Button, Modal,Form,Input,Select,DatePicker,Checkbox,TimePicker, Card,} from "antd";
 import { EditOutlined, DeleteOutlined, PlusOutlined } from "@ant-design/icons";
 import { LeftOutlined } from "@ant-design/icons";
 import { BASE_URL } from "../../constants/constants";
