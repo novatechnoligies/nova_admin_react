3f1d4b330f02645b6661f2f80ae00f2db80b5956
Santosh <78397565+santoshsh2307@users.noreply.github.com>
2024-01-28 04:23:14 +0530

    Access managent and get api save and tested

--- admin/src/App.js
+++ admin/src/App.js
@@ -10,10 +10,7 @@ import {
   ShoppingOutlined,
   IdcardOutlined,
 } from "@ant-design/icons/lib/icons";
-import ShopTable from "./components/shop_components/ShopTable";
-import SpaTable from "./components/spa_components/SpaTable";
 import LabTable from "./components/lab_components/LabTable";
-import CscTable from "./components/csc_components/CscTable";
 import ClinicTable from "./components/clinic_components/ClinicTable";
 import LoginPage from "./components/login_components/Login";
 // import Filter from './components/filter_componets/Filter';
@@ -30,6 +27,7 @@ import AppointmentDetails from "./components/consumers_appointments/AppointmentD
 import TestForm from "./components/result/TestForm";
 import axios from "axios";
 import { BASE_URL } from "./constants/constants";
+import AccessManagement from "./components/access_management/AccessManagement";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -169,7 +167,15 @@ function Header({ onLogout, profilePhoto }) {
 
 function SlideMenu({ location, navigate, onLogout }) {
   const selectedKeys = [location.pathname];
+  const storedUserData = sessionStorage.getItem("userData");
+  const userDataObject = JSON.parse(storedUserData);
+  const accessPermissionItems = userDataObject.accePermissions[0].items;
+   
+  const itemsArray = JSON.parse(accessPermissionItems);
+  itemsArray.some(item => item.label === "csc" && item.isOn);
+  //alert(itemsArray.some(item => item.label === "csc" && item.isOn));
 
+  const isHideClinic = itemsArray.some(item => item.label === "csc" && item.isOn);
   return (
     <div className="App">
       <Menu
@@ -189,34 +195,20 @@ function SlideMenu({ location, navigate, onLogout }) {
         }}
         items={[
           { label: "Home", key: "/", icon: <HomeOutlined /> },
+          { label: "Access Managemnt", key: "/accessManagement"},
           { label: "Dashboard", key: "/dash", icon: <DashboardOutlined /> },
           { label: "Appointments", key: "Acard", icon: <IdcardOutlined /> },
-          { label: "Lab", key: "/lab", icon: <ShopOutlined /> },
-          { label: "Spa", key: "/spa", icon: <ShopOutlined /> },
-          { label: "Shalon", key: "/shalon", icon: <ShopOutlined /> },
-          { label: "CSC", key: "/csc", icon: <ShopOutlined /> },
-          { label: "Clinic", key: "/clinic", icon: <ShopOutlined /> },
-          {
-            label: "Consumer",
-            key: "/consumer",
-            icon: <ShoppingOutlined />,
+           { label: "Lab", key: "/lab", icon: <ShopOutlined /> },
+          isHideClinic ? null : { label: "Clinic", key: "/clinic", icon: <ShopOutlined /> },
+          {label: "Consumer",key: "/consumer",icon: < ShoppingOutlined />,
             children: [
               { label: "Active", key: "/active", icon: <ShopOutlined /> },
               { label: "Inactive", key: "/inactive", icon: <ShopOutlined /> },
-              {
-                label: "New Consumer Account",
-                key: "/newconsumeraccount",
-                icon: <ShopOutlined />,
-              },
-            ],
-          },
+              {label: "New Consumer Account",key: "/newconsumeraccount",icon: <ShopOutlined />,},
+            ],},
           { label: "Profile", key: "/profile", icon: <ProfileOutlined /> },
-          {
-            label: "Sign Out",
-            key: "/logout",
-            icon: <SettingOutlined />,
-            danger: true,
-          },
+          
+          {label: "Sign Out",key: "/logout",icon: <SettingOutlined />,danger: true, },
         ]}
       />
     </div>
@@ -229,83 +221,20 @@ function Content() {
       <Routes>
         <Route path="/" element={<div>Dashboard</div>} />
         <Route path="/dash" element={<div>Dashboard</div>} />
-        <Route
-          path="/lab"
-          element={
-            <div className="d-flex flex-column align-items-center">
-              {" "}
-              <LabTable />
-            </div>
-          }
-        />
-        <Route
-          path="/Acard"
-          element={
-            <div className="d-flex flex-column align-items-center">
-              {" "}
-              <Acards />
-            </div>
-          }
-        />
-        <Route
-          path="/spa"
-          element={
-            <div className="d-flex flex-column align-items-center">
-              {" "}
-              <SpaTable />
-            </div>
-          }
-        />
-        <Route
-          path="/shalon"
-          element={
-            <div className="d-flex flex-column align-items-center">
-              {" "}
-              <ShopTable />
-            </div>
-          }
-        />
-        <Route
-          path="/csc"
-          element={
-            <div className="d-flex flex-column align-items-center">
-              {" "}
-              <CscTable />
-            </div>
-          }
-        />
-        <Route
-          path="/clinic"
-          element={
-            <div className="d-flex flex-column align-items-center">
-              {" "}
-              <ClinicTable />
-            </div>
-          }
-        />
+        <Route path="/lab" element={ <div className="d-flex flex-column align-items-center">{" "} <LabTable /> </div> } />
+        <Route  path="/Acard" element={ <div className="d-flex flex-column align-items-center">{" "} <Acards /> </div>} />
+        <Route path="/clinic" element={ <div className="d-flex flex-column align-items-center"> {" "} <ClinicTable /></div>}/>
         <Route path="/consumer" element={<div>Consumer</div>} />
         <Route path="/profile" element={<div>Profile</div>} />
         <Route path="/logout" element={<div>Logout</div>} />
         <Route path="/active" element={<div>Active</div>} />
         <Route path="/inactive" element={<div>Inactive</div>} />
         <Route path="/AppChildCards/:id" element={<More />} />
-        <Route
-          path="/newconsumeraccount"
-          element={
-            <div>
-              <NewConsumer />
-            </div>
-          }
-        />
-        <Route
-          path="/appointment-booking"
-          element={<AppointmentBookingPage />}
-        />
-        <Route
-          path="/appointment-details/:appointmentId"
-          element={<AppointmentDetails />}
-        />
+        <Route path="/newconsumeraccount" element={ <div> <NewConsumer />  </div>}/>
+        <Route path="/appointment-booking" element={<AppointmentBookingPage />} />
+        <Route path="/appointment-details/:appointmentId"  element={<AppointmentDetails />}/>
         <Route path="/report" element={<TestForm />} />
+        <Route path="/accessManagement" element={<AccessManagement />} />
       </Routes>
     </div>
   );
