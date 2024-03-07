6c11644c53a6dbeafbf44e09652f6d93f788aec3
Rakeshkotin17 <141311946+Rakeshkotin17@users.noreply.github.com>
2024-03-07 13:46:49 +0530

    NOVACARD-214  Created a consumer account button in Appointment  Booking screen
    NOVACARD-214  Created a consumer account button in Appointment  Booking screen

--- admin/src/components/consumers_appointments/ExistingAccountSearch.js
+++ admin/src/components/consumers_appointments/ExistingAccountSearch.js
@@ -3,6 +3,7 @@ import { Form, Input, Button, Table, Space } from 'antd';
 import { SearchOutlined, PlusOutlined } from '@ant-design/icons';
 import { BASE_URL } from "../../constants/constants";
 import axios from "axios";
+import { Link } from 'react-router-dom';
 import qs from 'qs';
 
 const ExistingAccountSearch = ({ onSelectAccount }) => {
@@ -63,13 +64,23 @@ const ExistingAccountSearch = ({ onSelectAccount }) => {
   return (
     <div>
       <Form form={form} onFinish={getConsumerListByPhone} layout="inline">
-        <Form.Item name="searchQuery">
+        <Form.Item name="searchQuery"   style={{ width: '150px', marginRight: "10px" }}>
           <Input placeholder="Search by name or contact number" />
         </Form.Item>
         <Form.Item>
-          <Button type="primary" htmlType="submit" icon={<SearchOutlined />}>
+          <Button type="primary" 
+          htmlType="submit" icon={<SearchOutlined />}style={{ width: "90px",  marginRight: "10px"  }}>
             Search
           </Button>
+          <Link to="/newconsumeraccount">
+            <Button
+              type="primary"
+              style={{ float: "right", marginLeft: "5px", width: "206px"}}
+              icon={<PlusOutlined />}
+            >
+              Create Consumer Account
+            </Button>
+          </Link>
         </Form.Item>
       </Form>
 
